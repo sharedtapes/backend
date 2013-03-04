4fd9759f7e92a0631cc2bdbeb2446f4a30c59243
Drew Harris <drewbharris@gmail.com>
2013-03-04 12:15:05 -0800

    more database changes. wrote tests. now have a method to do a full insert mixtape transaction

--- test/db.js
+++ test/db.js
@@ -7,42 +7,74 @@ var Db = require('../lib/db.js'),
 
 nconf.file(__dirname + '/../config.json');
 var myDatabase = Db.create("tcp://" +
-	nconf.get('postgresUser') + ":" +
-	nconf.get('postgresPassword') + "@" +
-	nconf.get('postgresHost') + ":" +
-	nconf.get('postgresPort'));
+    nconf.get('postgresUser') + ":" +
+    nconf.get('postgresPassword') + "@" +
+    nconf.get('postgresHost') + ":" +
+    nconf.get('postgresPort'));
+
+// just a quick fail function
+var fail = function(err, done){
+    assert.equal(null, err);
+    done();
+};
 
 describe('Database', function(){
 
-	// Do this before running any tests.
-	before(function(done){
-		myDatabase.connect().then(function(){
-			myDatabase.createTables().then(function(){
-				return done();
-			}, function(err){
-				console.log('error: ' + err);
-				myDatabase.disconnect();
-			});
-		}, function(err){
-			console.log('error: ' + err);
-			myDatabase.disconnect();
-		});
-	});
+    // Do this before running any tests.
+    before(function(done){
+        myDatabase.connect().then(function(){
+            myDatabase.createTables().then(function(){
+                return done();
+            }, function(err){
+                console.log('error: ' + err);
+                myDatabase.disconnect();
+            });
+        }, function(err){
+            console.log('error: ' + err);
+            myDatabase.disconnect();
+        });
+    });
 
-	// Do this after running all the tests.
-	after(function(done){
-		myDatabase.deleteTables().then(function(){
-			myDatabase.disconnect();
-			return done();
-		}, function(err){
-			console.log('error: ' + err);
-			myDatabase.disconnect();
-			return done();
-		});
-	});
+    // Do this after running all the tests.
+    after(function(done){
+        myDatabase.deleteTables().then(function(){
+            myDatabase.disconnect();
+            return done();
+        }, function(err){
+            console.log('error: ' + err);
+            myDatabase.disconnect();
+            return done();
+        });
+    });
 
-	it("should say yo", function(done){
-		done();
-	});
+    it("should insert a mixtape", function(done){
+        myDatabase.insertMixtape({
+            songs: [{
+                id: '123abc',
+                title: 'Thought Of You',
+                artist: 'Justin Bieber',
+                album: 'something',
+                url: 'http://awesome.com/awesome.mp3',
+                similar_artists: ['Germany Germany', 'Brendan Leddy'],
+                source: 'http://awesome.com'
+            }, {
+                id: '456def',
+                title: 'Baby',
+                artist: 'Justin Bieber',
+                album: 'something',
+                url: 'http://awesome.com/awesome2.mp3',
+                similar_artists: ['Germany Germany', 'Drew Harris'],
+                source: 'http://awesome.com'
+            }],
+            title: 'untitled',
+            author: 'anonymous',
+            created: '2013-03-04T17:16:37.325Z'
+        }).then(function(response){
+            done();
+        }, function(err){
+            console.log(err);
+            fail(err, done);
+        });
+    });
 
 });
