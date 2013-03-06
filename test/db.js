bc46342d2ba15b510916857d9961a6408496e079
Drew Harris <drewbharris@gmail.com>
2013-03-06 13:50:21 -0800

    Schema changes.  Added song source and locking.  Adding update.

--- test/db.js
+++ test/db.js
@@ -66,19 +66,19 @@ describe('Database', function(){
                 album: 'something',
                 url: 'http://awesome.com/awesome.mp3',
                 similar_artists: ['Germany Germany', 'Brendan Leddy'],
-                source: 'http://awesome.com'
+                sources: ['http://awesome2.com']
             }, {
                 id: '456def',
                 title: 'Baby',
                 artist: 'Justin Bieber',
                 album: 'something',
                 url: 'http://awesome.com/awesome2.mp3',
-                similar_artists: ['Germany Germany', 'Drew Harris'],
-                source: 'http://awesome.com'
+                similar_artists: ['Germany Germany', 'Drew Harris']
             }],
             title: 'untitled',
             author: 'anonymous',
-            created: '2013-03-04T17:16:37.325Z'
+            created: '2013-03-04T17:16:37.325Z',
+            locked: false
         }).then(function(response){
             done();
         }, function(err){
@@ -89,11 +89,44 @@ describe('Database', function(){
 
     it("should get a mixtape", function(done){
         myDatabase.getMixtape(1).then(function(response){
+            console.log(response);
             done();
         }, function(err){
             console.log(err);
             fail(err, done);
         });
     });
+
+    // it("should update a mixtape", function(done){
+    //     myDatabase.updateMixtape({
+    //         title: 'my mix',
+    //         author: 'anonymous',
+    //         created: '2013-03-04T17:16:37.325Z',
+    //         modified: '2013-03-07T04:07:10.216Z',
+    //         locked: false,
+    //         similarArtists: [ 'Germany Germany',
+    //              'Brendan Leddy',
+    //              'Germany Germany',
+    //              'Drew Harris'],
+    //         songs: [{
+    //             id: '456def',
+    //             song_id: 2,
+    //             title: 'Baby',
+    //             artist: 'Justin Bieber',
+    //             album: 'something',
+    //             url: 'http://awesome.com/awesome2.mp3',
+    //             similar_artists: ['Germany Germany', 'Drew Harris'],
+    //             source: 'http://awesome.com'
+    //         }, {
+    //             id: '789hij',
+    //             title: 'Another Song',
+    //             artist: 'Justin Bieber',
+    //             album: 'something',
+    //             url: 'http://awesome.com/awesome3.mp3',
+    //             similar_artists: ['Germany Germany', 'Nathan Willson'],
+    //             source: 'http://awesome.com'
+    //         }]
+    //     });
+    // });
 
 });
