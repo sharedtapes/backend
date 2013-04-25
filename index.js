ee28062b57be5f3b6cede2d752a40cf959b277ae
Drew Harris <drewbharris@gmail.com>
2013-04-25 07:31:51 -0700

    add comments

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var request = require('superagent'),
     app = require('./lib/app'),
     port = 12370;
 
+// Initialize the application and start the server
 app.start(port, function(){
     console.log('listening on ' + port);
 });
